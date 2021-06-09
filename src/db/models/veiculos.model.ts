0e1b0419d2c445d9e9c58a90ee2e801591b011cd
Fabio Santos <fabioprogramadorti.gmail.com>
2021-06-09 14:16:22 -0300

    Add VeiculoSchema

---
+++ src/db/models/veiculos.model.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const VeiculoSchema = new mongoose.Schema({
+  placa: {
+    type: String,
+    required: true
+  },
+  chassi: {
+    type: String,
+    required: true
+  },
+  renavam: {
+    type: String,
+    required: true
+  },
+  modelo: {
+    type: String,
+    required: true
+  },
+  marca: {
+    type: String,
+    required: true
+  },
+  ano: {
+    type: Number,
+    required: true
+  },
+}, { timestamps: { createdAt: 'createdAt' } })
+
+export default mongoose.model('veiculos', VeiculoSchema)
